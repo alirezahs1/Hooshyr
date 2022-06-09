800d36ea80d84ab97e1a1cbb669ff6c2a8db0f6e
Alireza Hooshyar <hooshyar.net@gmail.com>
2022-06-09 12:02:51 +0430

    change meta tags | exported

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,8 +27,8 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 				<meta name="description" content="Hooshyr is a web developer based in Tehran, Iran. He is a passionate about web development." />
 				{/* og tags */}
-				<meta property="og:title" content="Home | Hooshyr" />
-				<meta property="og:description" content="Hooshyr is a web developer based in Tehran, Iran. He is a passionate about web development." />
+				<meta property="og:title" content="Alireza Hooshyar" />
+				<meta property="og:description" content="Alireza Hooshyar is a web developer based in Tehran, Iran. He is a passionate about web development." />
 				<meta property="og:image" content="https://hooshyr.com/media/logo.png" />
 				<meta property="og:url" content="https://hooshyr.com" />
 				<meta property="og:type" content="website" />
